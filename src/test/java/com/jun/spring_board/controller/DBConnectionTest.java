be8d7902efffa673d254127294a0e3c1e343a8bd
LeeJun <ljun1118@ks.ac.kr>
2021-12-01 12:35:44 +0900

    dependency 추가 및 폴더 구성 변경

---
+++ src/test/java/com/jun/spring_board/controller/DBConnectionTest.java
@@ -0,0 +1,32 @@
+package com.jun.spring_board.controller;
+
+
+/*import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)*/
+//@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*-context.xml"})
+//@ContextConfiguration(locations = {"classpath:/spring/"})
+/*
+public class DBConnectionTest {
+
+    @Autowired
+    private DataSource ds;
+
+    @Test
+    public void testConnection() {
+        try (Connection con = ds.getConnection()) {
+            System.out.println(("DB 연결 성공 : " + con));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+*/
